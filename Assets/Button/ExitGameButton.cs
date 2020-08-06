6b97f0ecf912ae02b9b0b77da9e8d32af6c8e01d
shunmame <ac6328mats@g.kumamoto-nct.ac.jp>
2020-08-06 21:16:35 +0900

    feature: game exit

--- Assets/Button/ExitGameButton.cs
+++ Assets/Button/ExitGameButton.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ExitGameButton : MonoBehaviour
 {
+    [SerializeField]
+    Canvas ExitGameCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ExitGameCanvas.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnDialogOpen()
+    {
+        ExitGameCanvas.enabled = true;
+    }
+
+    public void OnExit(){
+        Application.Quit();
+    }
+
+    public void OnCancel(){
+        ExitGameCanvas.enabled = false;
     }
 }
