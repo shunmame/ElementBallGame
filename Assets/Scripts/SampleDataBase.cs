de81abbc3580924da706d56f6547b4774ddfe5a7
tsurumotonaoki <tsurumoto.naoki@gmail.com>
2020-08-09 15:15:03 +0900

    create sqlite env

---
+++ Assets/Scripts/SampleDataBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleDataBase : MonoBehaviour {
+
+    public SqliteDatabase sqlDB;
+
+    void Start(){
+　　　　　sqlDB = new SqliteDatabase("ElementBallGame.db");
+　　　　　string query = "SELECT * FROM example";
+　　　　　var dt = sqlDB.ExecuteQuery(query);
+
+        string name;
+        int dummy;
+        foreach(DataRow dr in dt.Rows){
+            name = (string)dr["name"];
+            dummy = (int)dr["dummy"];
+            Debug.Log("name:" + name.ToString());
+            Debug.Log("dummy:" + dummy);
+        }
+    }
+}
