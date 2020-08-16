34a49abad737d4ad854ed5b2b9c2f0f0c94b7a2c
aisyou <mi9103ishi@g.kumamoto-nct.ac.jp>
2020-08-16 17:42:08 +0900

    score display

---
+++ Assets/Score/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    //public int totalscore;
+    private Text targetText;
+    public string score = "---";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //totalsocre=int.Parse(score1) + int.Parse(score2) + int.Parse(score3) + int.Parse(score4) + int.Parse(score5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.targetText = this.GetComponent<Text>();
+        this.targetText.text =score;
+    }
+}
---
+++ Assets/Score/Score4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score4 : MonoBehaviour
+{
+    private Text targetText;
+    public string score4 = "---";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.targetText = this.GetComponent<Text>();
+        this.targetText.text = score4;
+    }
+}
