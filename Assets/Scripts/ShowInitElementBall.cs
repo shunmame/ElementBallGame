6029a9228a6a541ed8cf1fd771f96923b99bf2aa
shunmame <ac6328mats@g.kumamoto-nct.ac.jp>
2020-08-21 23:42:10 +0900

    add: element limit number

--- Assets/Scripts/ShowInitElementBall.cs
+++ Assets/Scripts/ShowInitElementBall.cs
@@ -1,29 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowInitElementBall : MonoBehaviour
 {
-    GameObject[] ElementBalls;
+    GameObject[] ElementBalls, ElementLimitTexts;
 
     // Start is called before the first frame update
     void Start()
     {
-        var BallPos = new List<Vector3>(){ 
-            new Vector3(-0.9f, 0.18f, -1.2f),  // 左上
-            new Vector3(-0.9f, 0.18f, -1.0f),  // 真ん中上
-            new Vector3(-0.9f, 0.18f, -0.8f),  // 右上
-            new Vector3(-0.9f, 0.06f, -1.2f),  // 左下
-            new Vector3(-0.9f, 0.06f, -1.0f),  // 真ん中下
-            new Vector3(-0.9f, 0.06f, -0.8f)   // 右下
-        };
-
-        var PosName = new List<string>(){"UpperLeft", "UpperCenter", "UpperRight", 
-                                         "LowerLeft", "LowerCenter", "LowerRight"
-                                        };
-        
         // 元素オブジェクトを取得
         ElementBalls = GameObject.FindGameObjectsWithTag("ElementBall");
+        // 残り個数表示テキストオブジェクトを取得
+        ElementLimitTexts = GameObject.FindGameObjectsWithTag("ElementLimitText");
         // Hのマテリアル取得
         Material elementH_material = Resources.Load("Material/element_H") as Material;
 
@@ -31,12 +21,8 @@ public class ShowInitElementBall : MonoBehaviour
         {
             // Hのテクスチャをはる
             ElementBalls[i].GetComponent<Renderer>().material = elementH_material;
-
-            // ここ使ってた
-            // GameObject ElementBallPf = (GameObject)Resources.Load("ElementBallPf");
-            // GameObject ElementBall = (GameObject)Instantiate(ElementBallPf, BallPos[i], Quaternion.identity);
-            // ElementBall.transform.parent = GameObject.Find("Ball").transform;
-            // ElementBall.name = PosName[i] + "Element";
+            // 残り個数を代入
+            ElementLimitTexts[i].GetComponent<Text>().text = "×2";
         }
     }
 
