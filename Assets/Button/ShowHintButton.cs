38d1fd247496c3b95ff2941b092c5bee0833312f
shunmame <ac6328mats@g.kumamoto-nct.ac.jp>
2020-08-23 21:47:45 +0900

    add: RightCanvas feature

---
+++ Assets/Button/ShowHintButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHintButton : MonoBehaviour
+{
+    [SerializeField]
+    Text HintText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HintText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHintOpen()
+    {
+        HintText.enabled = true;
+    }
+}
