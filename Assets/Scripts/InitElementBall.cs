d95750df05004d418e1cf15033b23d242cd78fda
shunmame <ac6328mats@g.kumamoto-nct.ac.jp>
2020-08-23 17:27:28 +0900

    add: Next Monster

---
+++ Assets/Scripts/InitElementBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitElementBall : MonoBehaviour
+{
+    GameObject[] ElementBalls, ElementLimitTexts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 元素オブジェクトを取得
+        ElementBalls = GameObject.FindGameObjectsWithTag("ElementBall");
+        // 残り個数表示テキストオブジェクトを取得
+        ElementLimitTexts = GameObject.FindGameObjectsWithTag("ElementLimitText");
+        // Hのマテリアル取得
+        Material elementH_material = Resources.Load("Material/element_H") as Material;
+
+        for(int i = 0; i < 6; i++)
+        {
+            // Hのテクスチャをはる
+            ElementBalls[i].GetComponent<Renderer>().material = elementH_material;
+            // 残り個数を代入
+            ElementLimitTexts[i].GetComponent<Text>().text = "×2";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/InitNextMonster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitNextMonster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object MonsterPf_H = Resources.Load("Model/Monster/h2");
+        GameObject Monster = (GameObject)Instantiate(MonsterPf_H, new Vector3(0.9f, -0.2f, -1.2f), Quaternion.Euler(0, 90, 0));
+        Monster.transform.localScale = new Vector3(10, 10, 10);
+        Monster.transform.parent = GameObject.Find("NextMonster").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
